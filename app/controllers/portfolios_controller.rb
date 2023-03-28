ca5fcbcd33547752a0ea288b88e0817de5b61b77
ajay <ajay.kumar@kpitechservices.com>
2023-03-28 19:46:29 +0530

    used destroy method

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,4 +37,12 @@ def update
   def show
     @portfolio_item=Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item=Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Record was successfully removed." }
+    end
+  end
 end
